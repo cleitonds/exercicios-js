3ed8d51c365636e1dba11c217d8cbac0172348e4
cleitonds <cleitoonsccp@gmail.com>
2022-01-07 00:29:46 -0300

    conitnuaçao das aulas

--- fundamentos/strings.js
+++ fundamentos/strings.js
@@ -1,4 +1,17 @@
+// string é um cadeia de delimitado por "", '' ou por crase. 
+
 const escola = "Cod3r"
 
 console.log(escola.charAt(4))
 console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat("!"))
+// console.log('Escola ' + escola + "!") outro exemplo de concatenação, como .log acima.
+console.log(escola.replace (3, 'e'))
+
+console.log('Ana, Maria, Pedro'.split(','))
--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -5,3 +5,5 @@ console.log(typeof qualquer)
 qualquer = 3.1516
 console.log(qualquer)
 console.log(typeof qualquer)
+
+// Evitar nomes genéricos e siglas
